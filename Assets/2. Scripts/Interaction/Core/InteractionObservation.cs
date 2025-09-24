3c256d5020866f9f8589f43e6871274b5d87cb30
alphapo040511 <alphapo05@gmail.com>
2025-09-24 10:44:29 +0900

    [Fix] 아이템 관찰 크기 조절 방식 변경

--- Assets/2. Scripts/Interaction/Core/InteractionObservation.cs
+++ Assets/2. Scripts/Interaction/Core/InteractionObservation.cs
@@ -8,18 +8,28 @@ using UnityEngine.Rendering.Universal;
 
 public class InteractionObservation : Interactable
 {
+    [Header("Camera & Volume Settings")]
     public Camera mainCamera;
     public Volume volume;
     private DepthOfField dof;
 
+
+    [Header("Observation Settings")]
+    public float focusDistance = 0.5f;
+    public float minSize = 0.5f;
+    public float maxSize = 2f;
+
+    // 위치 저장
     private Vector3 originPosition;
     private Quaternion originRatation;
-
     private Vector3 targetPosition;
-    private bool isObserved = false;
 
-    public float focusOffset = 0.5f;
-    private float currentOffset;
+    // 크기 저장
+    private Vector3 originSize;
+    private float sizeMultiplier;
+
+    private bool isObserving = false;
+    private bool isInitializing = false;
 
     private void Start()
     {
@@ -46,12 +56,34 @@ public class InteractionObservation : Interactable
     {
         base.Update();
 
-        if (isObserved == false) return;                      // 상호작용중이 아니면 return
+        if (isObserving == false) return;                       // 상호작용중이 아니면 return
 
-        targetPosition = mainCamera.transform.position + mainCamera.transform.forward * currentOffset;
+        if (Input.GetKeyDown(KeyCode.Escape))                   // ESC 키로 관찰 종료
+        {
+            ExitObseravtion();
+        }
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 5);
+        if (isInitializing) SetInitialPosition();               // 초기 위치에 도달할 때까지 이동
 
+        Rotate();
+        Zoom();
+    }
+
+    // 초기 위치 설정
+    void SetInitialPosition()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 5);  // 초기 위치로 부드럽게 이동
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
+        {
+            transform.position = targetPosition;
+            isInitializing = false;
+        }
+    }
+
+    // 오브젝트 회전
+    void Rotate()
+    {
         if (Input.GetMouseButton(0))
         {
             float rotX = -Input.GetAxis("Mouse X") * 3;
@@ -62,23 +94,20 @@ public class InteractionObservation : Interactable
             // 카메라 기준 상하 회전
             transform.Rotate(mainCamera.transform.right, -rotY, Space.World);
         }
-
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            ExitObseravtion();
-        }
-
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-
-        if (dof != null && Mathf.Abs(scroll) > 0.1f)
-        {
-            currentOffset += scroll * Time.deltaTime * 10f;
-            currentOffset = Mathf.Clamp(currentOffset, 0.2f, 0.8f);
-
-            dof.focusDistance.Override(currentOffset);
-        }
     }
 
+    // 확대 및 축소
+    void Zoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (Mathf.Abs(scroll) > 0.01f)
+        {
+            sizeMultiplier += scroll * Time.deltaTime * 20f;
+            sizeMultiplier = Mathf.Clamp(sizeMultiplier, minSize, maxSize);
+            transform.localScale = Vector3.Lerp(transform.localScale, originSize * sizeMultiplier, Time.deltaTime * 10f);
+        }
+    }
 
     protected override void Complete()
     {
@@ -86,24 +115,25 @@ public class InteractionObservation : Interactable
 
         GameManager.Instance.ChangeGameState(GameState.Menu);
 
-        isObserved = true;
+        isObserving = true;
+        isInitializing = true;
 
-        SetObservePosition();
+        // 위치 및 각도, 크기 저장
+        originPosition = transform.localPosition;
+        originRatation = transform.localRotation;
+        originSize = transform.localScale;
+
+        // 목표 크기 초기화
+        sizeMultiplier = 1f;
+
+        targetPosition = mainCamera.transform.position + mainCamera.transform.forward * focusDistance;  // 카메라 앞쪽에 위치
 
         if (dof != null)
-        {
-            dof.focusDistance.Override(focusOffset); // 포커스 거리 변경
-            currentOffset = focusOffset;
+        {            
+            dof.focusDistance.Override(focusDistance); // 포커스 거리 변경
         }
     }
 
-
-    void SetObservePosition()
-    {
-        originPosition = transform.localPosition;
-        originRatation = transform.localRotation;
-        targetPosition = mainCamera.transform.position + mainCamera.transform.forward * focusOffset;
-    }
 
     public void ExitObseravtion()
     {
@@ -111,16 +141,17 @@ public class InteractionObservation : Interactable
 
         GameManager.Instance.ChangeGameState(GameState.Playing);
 
-        isObserved = false;
+        isObserving = false;
+        isInitializing = false;
 
+        // 원래 위치로 복귀    
         transform.localPosition = originPosition;
         transform.localRotation = originRatation;
-
+        transform.localScale = originSize;
 
         if (dof != null)
         {
-            dof.focusDistance.Override(2f); // 포커스 거리 변경
-            currentOffset = 2f;
+            dof.focusDistance.Override(2f);                 // 포커스 거리 변경
         }
     }
 
