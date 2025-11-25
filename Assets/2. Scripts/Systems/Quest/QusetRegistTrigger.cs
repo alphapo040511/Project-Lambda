66f1f247795964d83844db9c2874d15267aeb397
alphapo040511 <alphapo05@gmail.com>
2025-11-25 13:27:20 +0900

    [Feat] 퀘스트 트리거, 재사용 처리 및 세이브 기능 추가

--- Assets/2. Scripts/Systems/Quest/QusetRegistTrigger.cs
+++ Assets/2. Scripts/Systems/Quest/QusetRegistTrigger.cs
@@ -1,16 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class QusetRegistTrigger : MonoBehaviour
+public class QusetRegistTrigger : SaveObject
 {
     public QuestDataSO targetQuest;
+    [Header("재사용 여부")] public bool reuseable = false;
+
+    public override void SetObjectState(string id, ObjectState state)
+    {
+        if( id ==  uniqueId)
+        {
+            this.state = state;
+            if(state == ObjectState.Disable)
+            {
+                this.enabled = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && State == ObjectState.Default)
         {
             QuestManager.Instance.RegistQuest(targetQuest.questId);
+
+            if(!reuseable)
+            {
+                state = ObjectState.Disable;
+                this.enabled = false;
+            }
         }
     }
 }
