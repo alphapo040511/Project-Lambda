aaf3181436f5be9e8988809abacb7336aa7c4a65
alphapo040511 <alphapo05@gmail.com>
2025-11-28 23:27:39 +0900

    [Feat] 퓨즈 박스(회전형 문) 까지 제작

---
+++ Assets/2. Scripts/Interaction/Objects/HingeObject.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HingeObject : MonoBehaviour, IOpenableObject
+{
+    public Vector3 closeRotation;
+    public Vector3 openRotation;
+    private Quaternion targetRotation
+    {
+        get
+        {
+            return isOpen ? Quaternion.Euler(openRotation) : Quaternion.Euler(closeRotation);
+        }
+    }
+
+    public float rotateSpeed = 1.5f;
+
+    public bool IsMoving => isMoving;
+
+    private bool isMoving = true;
+
+    public bool isOpen = false;
+
+    private void Update()
+    {
+        if(IsMoving)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, Time.deltaTime * rotateSpeed);
+
+            if(Quaternion.Angle(transform.localRotation, targetRotation) < 0.1f)
+            {
+                transform.localRotation = targetRotation;
+                isMoving = false;
+            }
+        }
+    }
+
+    public void Move()
+    {
+        isOpen = !isOpen;
+        isMoving = true;
+    }
+}
