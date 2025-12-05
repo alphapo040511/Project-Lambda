d58c26a71182a99417abb38bfb30307f9630a576
alphapo040511 <alphapo05@gmail.com>
2025-12-05 22:05:23 +0900

    [Feat] 퓨즈 보관함 이벤트 및 발견 이벤트 트리거 추가

---
+++ Assets/2. Scripts/CutScenes/ObjFocussingEventTrigger.cs
@@ -0,0 +1,78 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ObjFocussingEventTrigger : MonoBehaviour, ISaveObject
+{
+    [Header("저장 관련 설정")]
+    public string uniqueId;
+    public string UniqueId => uniqueId;
+
+    public ObjectState state;
+    public ObjectState State => state;
+
+    public UnityEvent onStartAction;
+    public UnityEvent onEndAction;
+
+    [Header("Event Settings")]
+    public QuestDataSO needQuest;
+    public CinemachineVirtualCamera actionCamera;
+    public float eventDuration = 3f;
+
+    [Header("Sound Settings")]
+    public AudioSource audioSource;
+    public AudioClip findSound;
+
+    public void SetObjectState(string id, ObjectState state)
+    {
+        if (id != uniqueId) return;
+        this.state = state;
+
+        switch(state)
+        {
+                case ObjectState.Disable:
+                break;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (state == ObjectState.Disable) return;
+        if (needQuest != null && QuestManager.Instance.currentQuestId != needQuest.questId) return;       // 목표 퀘스트 현재 퀘스트가 아닌경우 무시
+
+        if (actionCamera != null)
+        {
+            StartCoroutine(EventAction());
+        }
+        else
+        {
+            onStartAction?.Invoke();
+            onEndAction?.Invoke();
+            state = ObjectState.Disable;                // 사용 불가 상태로 변경
+        }
+    }
+
+    IEnumerator EventAction()
+    {
+        EventSystem.Instance.StartEvent();
+        onStartAction?.Invoke();
+        actionCamera.enabled = true;
+        
+        if(audioSource != null && findSound != null)
+        {
+            audioSource.PlayOneShot(findSound);
+        }
+
+        yield return new WaitForSeconds(eventDuration);
+
+        actionCamera.enabled = false;
+        EventSystem.Instance.EndEvent();
+        onEndAction?.Invoke();
+        state = ObjectState.Disable;                // 사용 불가 상태로 변경
+    }
+}
